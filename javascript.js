2e145fcac6fd4d9369d41476f81f9c6539ea9fce
Mikey <michael.parsons2323@gmail.com>
2022-11-14 16:07:14 -0600

    more ui work

---
+++ javascript.js
@@ -0,0 +1,34 @@
+let pressed = document.getElementById("7");
+pressed.onclick = function() {console.log("clicked")};
+
+function operate(op, x, y) 
+{
+    if (op == "+") {
+        return add(x, y);
+    } else if (op == "-") {
+        return sub(x, y);
+    } else if (op == "*")  {
+        return mult(x, y);
+    } else if (op == "/") {
+        return div(x, y);
+    } else {
+        return "error.";
+    }
+}
+
+function add(x, y) 
+{
+    return x + y;
+}
+function sub(x, y) 
+{
+    return x - y;
+}
+function mult(x, y) 
+{
+    return x * y;
+}
+function div(x, y) 
+{
+    return x / y;
+}
