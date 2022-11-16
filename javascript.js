4ab66421072f339de1a1333f485b8079be3c7a0c
Mikey <michael.parsons2323@gmail.com>
2022-11-16 00:32:36 -0600

    can click buttons to update the display...barely

--- javascript.js
+++ javascript.js
@@ -1,7 +1,43 @@
+var op;
 
 
-function clear_display() {
-    document.getElementById("display").innerHTML = "0";
+var nums = document.getElementsByClassName("num");
+for (let num of nums){
+    num.addEventListener("click", function(){
+        document.querySelector(".display").innerHTML += this.innerHTML
+        toX();
+        return;
+    });
+}
+
+var operators = document.getElementsByClassName("op");
+for (let operator of operators){
+    operator.addEventListener("click", function() {
+        document.querySelector(".display").innerHTML += " " + this.innerHTML + " ";
+        op = this.innerHTML;
+        document.getElementById("div").disabled = true;
+        document.getElementById("mult").disabled = true;
+        document.getElementById("sub").disabled = true;
+        document.getElementById("add").disabled = true;
+
+    })
+}
+
+/* Clear Button */
+document.querySelector("#clear").addEventListener("click", function(){
+    document.querySelector(".display").innerHTML = "";        document.getElementById("div").disabled = true;
+    document.getElementById("mult").disabled = false;
+    document.getElementById("sub").disabled = false;
+    document.getElementById("add").disabled = false;
+    document.getElementById("div").disabled = false;
+});
+
+function toX()
+{
+    var dispNum = document.querySelector(".display").innerHTML;
+    var first = parseFloat(dispNum);
+    console.log("x = " +  first);
+    console.log("op = " + op);
 }
 
 function operate(op, x, y) 
