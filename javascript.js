f596ff7bb196e9de5c8c810d75cb10c26ea3b34f
Mikey <michael.parsons2323@gmail.com>
2022-11-16 12:41:05 -0600

    calculator mostly works

--- javascript.js
+++ javascript.js
@@ -1,73 +1,67 @@
-var op;
-
-
+var op, x = "", y = "";
+var display = document.querySelector(".display");
+var operators = document.getElementsByClassName("op");
 var nums = document.getElementsByClassName("num");
+var clicked = false;
+
+/* Number Buttons */
 for (let num of nums){
     num.addEventListener("click", function(){
-        document.querySelector(".display").innerHTML += this.innerHTML
-        toX();
-        return;
+        display.innerHTML += this.innerHTML
+        if (clicked == false){
+            x += this.innerHTML;
+        } else {
+            y += this.innerHTML;
+        }
+        
     });
 }
 
-var operators = document.getElementsByClassName("op");
+/* Operators */
 for (let operator of operators){
-    operator.addEventListener("click", function() {
-        document.querySelector(".display").innerHTML += " " + this.innerHTML + " ";
+    operator.addEventListener("click", function(){
+        if (x != ""){
+            clicked = true;
+            y = "";
+        }
         op = this.innerHTML;
-        document.getElementById("div").disabled = true;
-        document.getElementById("mult").disabled = true;
-        document.getElementById("sub").disabled = true;
-        document.getElementById("add").disabled = true;
-
+        display.innerHTML = x + " " + op + " ";
     })
 }
 
 /* Clear Button */
 document.querySelector("#clear").addEventListener("click", function(){
-    document.querySelector(".display").innerHTML = "";        document.getElementById("div").disabled = true;
-    document.getElementById("mult").disabled = false;
-    document.getElementById("sub").disabled = false;
-    document.getElementById("add").disabled = false;
-    document.getElementById("div").disabled = false;
+    clicked = false;
+    display.innerHTML = "";
+    x = "";
+    y = "";
 });
 
-function toX()
-{
-    var dispNum = document.querySelector(".display").innerHTML;
-    var first = parseFloat(dispNum);
-    console.log("x = " +  first);
+/* Equals Button */
+document.querySelector("#equals").addEventListener("click", function(){
+    clicked = false;
+    x = parseFloat(x);
+    y = parseFloat(y);
+    console.log("x = " + x);
+    console.log("y = " + y);
     console.log("op = " + op);
-}
+    display.innerHTML = operate(op, x, y);
+    x = operate(op, x, y);
+});
+
 
 function operate(op, x, y) 
 {
     if (op == "+") {
-        return add(x, y);
+        x = (x + y);
     } else if (op == "-") {
-        return sub(x, y);
+        x = (x - y);
     } else if (op == "*")  {
-        return mult(x, y);
+        x = (x * y);
     } else if (op == "/") {
-        return div(x, y);
+        x = (x / y);
     } else {
         return "error.";
     }
-}
-
-function add(x, y) 
-{
-    return x + y;
-}
-function sub(x, y) 
-{
-    return x - y;
-}
-function mult(x, y) 
-{
-    return x * y;
-}
-function div(x, y) 
-{
-    return x / y;
+    return x;
 }
