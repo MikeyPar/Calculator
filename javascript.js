4e68123629a85c81fbc7e94c54fbccf73b79ccee
Mikey <michael.parsons2323@gmail.com>
2022-11-16 13:57:09 -0600

    better rounding and works with negative numbers

--- javascript.js
+++ javascript.js
@@ -1,4 +1,6 @@
-var op, x = "", y = "";
+var op 
+var x = "";
+var y = "";
 var display = document.querySelector(".display");
 var operators = document.getElementsByClassName("op");
 var nums = document.getElementsByClassName("num");
@@ -13,21 +15,30 @@ for (let num of nums){
         } else {
             y += this.innerHTML;
         }
-        
     });
 }
 
 /* Operators */
 for (let operator of operators){
     operator.addEventListener("click", function(){
-        if (x != ""){
+        if (x != "" || undefined){
             clicked = true;
             y = "";
         }
+        document.getElementById("dot").disabled = false;
         op = this.innerHTML;
         display.innerHTML = x + " " + op + " ";
-    })
+    });
 }
+
+document.querySelector("#sub").addEventListener("click", function(){
+    if (x == ""){
+        x += "-";
+    }
+
+});
+
+
 
 /* Clear Button */
 document.querySelector("#clear").addEventListener("click", function(){
@@ -35,19 +46,41 @@ document.querySelector("#clear").addEventListener("click", function(){
     display.innerHTML = "";
     x = "";
     y = "";
+    document.getElementById("dot").disabled = false;
 });
+
+/* Dot Button */
+document.getElementById("dot").addEventListener("click", function(){
+    if (x.includes(".")){
+        document.getElementById("dot").disabled = true;
+    }
+    if (x.includes(".")){
+        document.getElementById("dot").disabled = true;
+    }
+})
 
 /* Equals Button */
 document.querySelector("#equals").addEventListener("click", function(){
     clicked = false;
+    if (x == "" || undefined){
+        x = 0;
+    }
+    if (y == "" || undefined){
+        y = 0;
+    }
     x = parseFloat(x);
     y = parseFloat(y);
-    console.log("x = " + x);
-    console.log("y = " + y);
-    console.log("op = " + op);
-    display.innerHTML = operate(op, x, y);
+    display.innerHTML = operate(op, x, y)
     x = operate(op, x, y);
+    console.log(x);
+    console.log(y);
+    console.log(op)
+
+    x = x.toString();
+    y = y.toString();
+    document.getElementById("dot").disabled = false;
 });
+
 
 
 function operate(op, x, y) 
@@ -63,5 +96,6 @@ function operate(op, x, y)
     } else {
         return "error.";
     }
+    x = Math.round((x + Number.EPSILON) * 100) / 100;
     return x;
 }
